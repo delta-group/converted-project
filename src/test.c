9566e83e6b5a220b467902e6b85068f654250a07
Nick Kasten <kastencode@gmail.com>
2016-12-12 21:21:35 -0600

    fixing tabs

--- src/test.c
+++ src/test.c
@@ -6,30 +6,30 @@
 /************************************************************************/
 
 //--- PIN Assignments ----------------------------------------------------
-#define GREEN_PIN		4
-#define BLUE_PIN		5
-#define BUZZ_PIN		1
-#define TRIG_PIN		2	
-#define ECHO_PIN		4
+#define GREEN_PIN			4
+#define BLUE_PIN			5
+#define BUZZ_PIN			1
+#define TRIG_PIN			2
+#define ECHO_PIN			4
 
 //--- PIN Controls -------------------------------------------------------
-#define RED_ON          (PORTB |= (0x0F))
-#define RED_OFF         (PORTB &= ~(1<<RED_PIN))
-#define GREEN_ON        (PORTB |= (0x10))
-#define GREEN_OFF       (PORTB &= ~(1<<GREEN_PIN))
-#define YELLOW_ON		(PORTB |= (1<<RED_PIN) | (1<<GREEN_PIN))
-#define YELLOW_OFF		(PORTB &= (0x00))
+#define RED_ON				(PORTB |= (0x0F))
+#define RED_OFF				(PORTB &= ~(1<<RED_PIN))
+#define GREEN_ON			(PORTB |= (0x10))
+#define GREEN_OFF			(PORTB &= ~(1<<GREEN_PIN))
+#define YELLOW_ON			(PORTB |= (1<<RED_PIN) | (1<<GREEN_PIN))
+#define YELLOW_OFF			(PORTB &= (0x00))
 
-#define TRIG_ON			(PORTD |= (1<<TRIG_PIN))
-#define	TRIG_OFF		(PORTD &= ~(1<<TRIG_PIN))
-#define BUZZ_ON         (PORTB |= (1<<BUZZ_PIN))
-#define BUZZ_OFF        (PORTB &= ~(1<<BUZZ_PIN))
+#define TRIG_ON				(PORTD |= (1<<TRIG_PIN))
+#define	TRIG_OFF			(PORTD &= ~(1<<TRIG_PIN))
+#define BUZZ_ON				(PORTB |= (1<<BUZZ_PIN))
+#define BUZZ_OFF			(PORTB &= ~(1<<BUZZ_PIN))
 
 //--- Port Setup ---------------------------------------------------------
-#define OUTPUT_CONFIG   (DDRB |= (1<<RED_PIN) | (1<<GREEN_PIN) | (1<<BUZZ_PIN))
+#define OUTPUT_CONFIG		(DDRB |= (1<<RED_PIN) | (1<<GREEN_PIN) | (1<<BUZZ_PIN))
 
 //--- Clock/Timer Setup --------------------------------------------------
-#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
+#define CPU_PRESCALE(n)		(CLKPR = 0x80, CLKPR = (n))
 
 /************************************************************************/
 /*                     Device function definitions                      */
