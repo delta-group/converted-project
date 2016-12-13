a333d42ea873b60d8b936248ace2c4f71478cc4e
Nick Kasten <kastencode@gmail.com>
2016-12-12 21:30:55 -0600

    spaces spaces everywhere

--- src/test.c
+++ src/test.c
@@ -6,11 +6,11 @@
 /************************************************************************/
 
 //--- PIN Assignments ----------------------------------------------------
-#define GREEN_PIN			4
-#define BLUE_PIN			5
-#define BUZZ_PIN			1
-#define TRIG_PIN			2
-#define ECHO_PIN			4
+#define GREEN_PIN           4
+#define BLUE_PIN            5
+#define BUZZ_PIN            1
+#define TRIG_PIN            2
+#define ECHO_PIN            4
 
 //--- PIN Controls -------------------------------------------------------
 #define RED_ON              (PORTB |= (0x0F))
@@ -20,10 +20,10 @@
 #define YELLOW_ON           (PORTB |= (1<<RED_PIN) | (1<<GREEN_PIN))
 #define YELLOW_OFF          (PORTB &= (0x00))
 
-#define TRIG_ON				(PORTD |= (1<<TRIG_PIN))
-#define	TRIG_OFF			(PORTD &= ~(1<<TRIG_PIN))
-#define BUZZ_ON				(PORTB |= (1<<BUZZ_PIN))
-#define BUZZ_OFF			(PORTB &= ~(1<<BUZZ_PIN))
+#define TRIG_ON             (PORTD |= (1<<TRIG_PIN))
+#define	TRIG_OFF            (PORTD &= ~(1<<TRIG_PIN))
+#define BUZZ_ON             (PORTB |= (1<<BUZZ_PIN))
+#define BUZZ_OFF            (PORTB &= ~(1<<BUZZ_PIN))
 
 //--- Port Setup ---------------------------------------------------------
 #define OUTPUT_CONFIG		(DDRB |= (1<<RED_PIN) | (1<<GREEN_PIN) | (1<<BUZZ_PIN))
